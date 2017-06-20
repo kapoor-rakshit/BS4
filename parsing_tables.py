868a05f130924e5ae144e5b144bd760e6b16cf69
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2017-06-20 20:53:01 +0530

    Add files via upload

--- parsing_tables.py
+++ parsing_tables.py
@@ -1,5 +1,6 @@
 import bs4 as bs
 import urllib.request
+import pandas as pd
 
 site=urllib.request.urlopen("https://pythonprogramming.net/parsememcparseface/").read()
 soup=bs.BeautifulSoup(site,"lxml")
@@ -12,3 +13,11 @@ for table in tablelist:
 		for data in row.find_all("td"):
 			k.append(data.text)                      # list of data in each row
 		print(k)
+
+
+
+# table scrapping using pandas
+
+dataframes=pd.read_html("https://pythonprogramming.net/parsememcparseface/",header=0)  # make 0th row as header
+for dataframe in dataframes:
+	print(dataframe)
